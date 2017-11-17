ba9cbef339d6a2536c305693e8b2b057c9ed54cc
Renran Zhou <rz348@cornell.edu>
2017-11-17 09:32:22 -0500

    aaa

--- app/src/main/java/com/example/zhou/camera2/MainActivity.java
+++ app/src/main/java/com/example/zhou/camera2/MainActivity.java
@@ -19,90 +19,9 @@ import android.view.WindowManager;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.content.pm.ActivityInfo;
+import com.example.zhou.camera.R;
 
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
-public class VideoCapture extends Activity implements OnClickListener, SurfaceHolder.Callback {
-    MediaRecorder recorder;
-    SurfaceHolder holder;
-    boolean recording = false;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        recorder = new MediaRecorder();
-        initRecorder();
-        setContentView(R.layout.activity_main);
-
-        SurfaceView cameraView = (SurfaceView) findViewById(R.id.camera_preview);
-        holder = cameraView.getHolder();
-        holder.addCallback(this);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        cameraView.setClickable(true); 
-        cameraView.setOnClickListener(this);
-    }
-
-    private void initRecorder() {
-        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
-        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
-
-        CamcorderProfile cpHigh = CamcorderProfile
-                .get(CamcorderProfile.QUALITY_HIGH);
-        recorder.setProfile(cpHigh);
-        recorder.setOutputFile("/sdcard/videocapture_example.mp4");
-        recorder.setMaxDuration(50000); // 50 seconds
-        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
-    }
-
-    private void prepareRecorder() {
-        recorder.setPreviewDisplay(holder.getSurface());
-
-        try {
-            recorder.prepare();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            finish();
-        } catch (IOException e) {
-            e.printStackTrace();
-            finish();
-        }
-    }
-
-    public void onClick(View v) {
-        if (recording) {
-            recorder.stop();
-            recording = false;
-
-            // Let's initRecorder so we can record again
-            initRecorder();
-            prepareRecorder();
-        } else {
-            recording = true;
-            recorder.start();
-        }
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        prepareRecorder();
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        if (recording) {
-            recorder.stop();
-            recording = false;
-        }
-        recorder.release();
-        finish();
-    }
-}
